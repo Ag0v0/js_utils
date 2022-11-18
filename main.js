032fd42b72e258546d67bedb4fa56d84b6ca94b6
Ag <i-am-ag@foxmail.com>
2022-11-18 16:29:59 +0800

    00

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+ * @Description: 入口
+ * @Autor: Ag
+ * @Date: 2022-11-18 15:42:17
+ * @LastEditors: Ag
+ * @LastEditTime: 2022-11-18 15:43:57
+ */
+export * from "./lib";
+export * from "./lib/file";
